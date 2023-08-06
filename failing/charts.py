5a83baf63ff459efb89497058751a6460e7c4e42
edisonlimllk <115971906+edisonlimllk@users.noreply.github.com>
2023-08-06 20:56:14 +0800

    Develop dynamic_charts()

--- failing/charts.py
+++ failing/charts.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from utils import clear
+from stats import list_items
 import numpy as np
 import inquirer
 
@@ -41,8 +42,6 @@ def assignment_charts(array: np.ndarray):
 
 """
 dynamic_charts() lets the user dynamically generate their own desired charts from the data
-
-WORK IN PROGRESS
 """
 def dynamic_charts(array: np.ndarray, types: np.ndarray):
     global type_index
@@ -58,19 +57,30 @@ def dynamic_charts(array: np.ndarray, types: np.ndarray):
             xpoints = array[:,0]
             print("You selected 'Data across the years'\n")
             variable_input = inquirer.list_input("What kind of data do you want to see?",
-                                choices=[("Univariate (bar chart for a single datatype)",1),("Bivariate (single-line chart e.g.: Bus passengers per Bus)",2),("Multivariate (multi-line chart)",3),(return_message,4)],
+                                choices=[("Univariate (line chart for a single datatype)",1),("Bivariate (single-line chart e.g.: Bus passengers per Bus)",2),("Multivariate (multi-line chart)",3),(return_message,4)],
                                 carousel=True)
             match(variable_input):
-                # Chosen: Univariate (bar chart, single type)
+                # Chosen: Univariate (line chart, single type)
                 case(1):
                     clear()
-                    print("You selected 'Univariate (bar chart for a single datatype)'\n")
+                    print("You selected 'Univariate (line chart for a single datatype)'\n")
                     type_index = inquirer.list_input("Select type",
                                     choices=[(type, idx) for idx, type in enumerate(types)],
                                     carousel=True)
                     clear()
                     print(f"You selected '{types[type_index]}'\n")
                     ypoints = array[:,type_index+1]
+                    plt.plot(xpoints, ypoints,
+                            color=color, 
+                            alpha=opacity, 
+                            marker=marker, 
+                            linestyle=linestyle, 
+                            linewidth=linewidth,
+                            )
+                    plt.ylabel(types[type_index])
+                    plt.xlabel('Year')
+                    plt.title(f"No. of {types[type_index].lower()} crossing over the years", fontdict = heading_font)
+                    plt.show()
                 # Chosen: Bivariate (single-line chart e.g.: Bus passengers per Bus)
                 case(2):
                     clear()
@@ -89,7 +99,19 @@ def dynamic_charts(array: np.ndarray, types: np.ndarray):
                                             choices=[(type, idx) for idx, type in enumerate(types) if idx != first_type_index],
                                             carousel=True)
                     clear()
-                    input(f"You have selected:\n\n{types[first_type_index]} per {types[second_type_index]}")
+                    print(f"You have selected:\n\n{types[first_type_index]} per {types[second_type_index]}.\n")
+                    ypoints =array[:,first_type_index+1]/array[:,second_type_index+1]
+                    plt.plot(xpoints, ypoints,
+                            color=color, 
+                            alpha=opacity, 
+                            marker=marker, 
+                            linestyle=linestyle, 
+                            linewidth=linewidth,
+                            )
+                    plt.ylabel(f"{types[first_type_index]} per {types[second_type_index]}")
+                    plt.xlabel('Year')
+                    plt.title(f"No. of {types[first_type_index]} per {types[second_type_index]} crossing over the years", fontdict = heading_font)
+                    plt.show()
                 # Chosen: Multivariate (multi-line chart, multiple types)
                 case(3):
                     clear()
@@ -97,35 +119,65 @@ def dynamic_charts(array: np.ndarray, types: np.ndarray):
                     multivariate_types = inquirer.checkbox("Select variables",
                                             choices=[(type, idx) for idx, type in enumerate(types)],
                                             carousel=True)
-                    input(multivariate_types)
+                    for i in multivariate_types:
+                        plt.plot(array[:,i+1],
+                            alpha=opacity, 
+                            marker=marker, 
+                            linestyle='solid', 
+                            linewidth=linewidth,
+                            label=types[i]
+                            )
+                    plt.plot(xpoints=xpoints)
+                    plt.ylabel(list_items([types[i] for i in multivariate_types]))
+                    plt.xlabel('Year')
+                    plt.title("Vehicle crossings over the years", fontdict = heading_font)
+                    plt.legend()
+                    plt.show()
                 # Chosen: return to view charts menu
                 case(4):
                     return
-
         # Chosen: data for a given year
         case(2):
             clear()
             print("You selected 'Data for a given year'.\n")
+            print(f"Available years: {array[0,0]} - {array[-1,0]}.\n")
+            year_input = inquirer.list_input("What year would you like to view?",
+                            choices=[i for i in range(array[0,0], array[-1,0]+1)],
+                            carousel=True)
             chart_input = inquirer.list_input("What kind of chart do you want to see?",
                                 choices=[("Bar chart (comparison)",1),("Pie chart (composition)",2),(return_message,3)],
                                 carousel=True)
+            print(f"{year_input}, {chart_input}")
             match(chart_input):
                 # Chosen: Bar chart (comparison)
                 case(1):
                     clear()
                     print("You selected 'Bar chart (comparison)'")
+                    plt.bar(types, 
+                            height=array[year_input%100,1:],
+                            color = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange']
+                            )
+                    plt.title(f"Crossing data in {year_input}", fontdict = heading_font)
+                    plt.show()
                 # Chosen: Pie chart (composition)
                 case(2):
                     clear()
                     print("You selected 'Pie chart (composition)'")
+                    wedgeprops = {"linewidth": 1, 'width':1, "edgecolor":"k"}
+                    plt.pie(array[year_input%100,1:], 
+                            labels=types,autopct="%0.2f%%", 
+                            explode=[0.1 for x in array[year_input%100,1:]],
+                            shadow=True,
+                            wedgeprops=wedgeprops,
+                            radius = 1.1,
+                            )
+                    plt.title(f"Crossings data in {year_input}", fontdict = heading_font)
+                    plt.legend()
+                    plt.show()
                 # Chosen: return to view charts menu
                 case(3):
                     return
-            print(f"Available years: {array[0,0]} - {array[-1,0]}.\n")
-            year_input = inquirer.list_input("What year would you like to view?",
-                            choices=[i for i in range(array[0,0], array[-1,0]+1)],
-                            carousel=True)
-            input(year_input)
+
         # Chosen: return to view charts menu
         case(3):
             return

534b35c8a4fd4e25a90d91f6eb2334b7cf667af8
SereneReaper <2100436A@student.tp.edu.sg>
2023-08-12 16:50:25 +0800

    Add Sounds, sprites and wool collection

--- welcome.py
+++ welcome.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget, QDesktopWidget
+from PyQt5.QtMultimedia import QMediaPlayer
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import Qt
+from utils import play_audio
 import sys
 import qdarktheme
 
@@ -20,11 +22,24 @@ class ImageDisplayApp(QMainWindow):
         self.image_label.setAlignment(Qt.AlignCenter)
         
         self.proceed_button = QPushButton('Select .csv file', self)
-        self.proceed_button.setStyleSheet(
-            "background-color: darkgreen; color: white; font-size: 60px; padding: 10px 20px; border-radius:30px"
-        )
+        proceed_button_stylesheet =  """
+                                QPushButton {
+                                    background-color: darkgreen; 
+                                    color: white; 
+                                    font-size: 60px; 
+                                    padding: 10px 10px; 
+                                }
+
+                                QPushButton:hover {
+                                    background-color: #26522d;
+                                    font-size: 80px;
+                                    }
+                                """
+        self.proceed_button.setStyleSheet(proceed_button_stylesheet)
         self.proceed_button.setFixedHeight(150)  # Adjust the height as needed
+        self.proceed_button.setCursor(Qt.PointingHandCursor)
         self.proceed_button.clicked.connect(self.on_proceed)
+        self.media_player = QMediaPlayer()
 
         self.layout = QVBoxLayout(self.central_widget)
         self.layout.addWidget(self.image_label)
@@ -35,13 +50,14 @@ class ImageDisplayApp(QMainWindow):
         self.image_label.setPixmap(self.image_pixmap)
 
     def on_proceed(self):
+        play_audio("click")
         self.close()
 
     def center_window(self):
         screen_geometry = QDesktopWidget().screenGeometry()
         window_geometry = self.geometry()
         x = (screen_geometry.width() - window_geometry.width()) // 2
-        y = (screen_geometry.height() - window_geometry.height()) // 4
+        y = (screen_geometry.height() - window_geometry.height()) // 8
         self.move(x, y)
 
 def welcome_window():
@@ -49,5 +65,6 @@ def welcome_window():
     qdarktheme.setup_theme("light")
     ex = ImageDisplayApp()
     ex.center_window()
+    ex.setFixedSize(700,850)
     ex.show()
     app.exec_()
